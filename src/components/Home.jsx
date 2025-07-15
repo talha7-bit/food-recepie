cbf3833325e0af597fcdc6b5ec54099e8a11f859
talha7-bit <talha7.webdev@gmail.com>
2025-07-14 19:54:03 -0700

    added loading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import Card from './Card';
+import { LoaderIcon } from 'lucide-react';
 
 const Home = () => {
     const[data,setdata]=useState([]);
     const[search,setsearch]=useState("");
     const[error,seterror]=useState("");
+    const[loading,setloading]=useState("");
+    const[load,setload]=useState(false);
 
     const fetchd=async(query="cake")=>{
+      setload(true);
     try{
         const response=await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
         const result=await response.json();
@@ -25,10 +29,15 @@ const Home = () => {
     
     }catch(error){
       seterror("something went wrong please try again")
+    }finally{
+      setload(false);
     }
   }
     useEffect(()=>{
-        fetchd();
+      setloading("loading...")
+        fetchd().finally(()=>{
+          setloading("");
+        })
     },[])
     const handlesearch=(e)=>{
       e.preventDefault();
@@ -37,16 +46,17 @@ const Home = () => {
       
     }
   return (
-    <div>
+    <div className='mt-3'>
       <form onSubmit={handlesearch} className='flex items-center mx-auto'>
         <input type='text' placeholder='search by name' value={search} onChange={(e)=>
           setsearch(e.target.value)
         } className='border rounded px-4 py-1 '/>
-        <button className='mx-1 text-white bg-orange-500 px-4 py-1 rounded-xl' type='submit'>Search</button>
+        <button className='mx-1 text-white bg-orange-500 px-4 py-1 rounded-xl' type='submit'>{load ? <LoaderIcon className='w-4 h-4 animate-spin'/>:" "} Search</button>
         </form>
         {error && <div className='text-red-500 text-center mt-1'>
           {error}
           </div>}
+          {loading && <h3 className='flex items-center justify-center mt-60'>{loading}</h3>}
     <Card items={data}/>  
     </div>
   )
